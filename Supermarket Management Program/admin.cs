460d62fd3866ea46cf89a04e409199412955b722
Youssef Harby <youssif2371@gmail.com>
2025-08-22 05:31:27 +0300

    Add admin.cs

---
+++ Supermarket Management Program/admin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_Management_Program
+{
+    internal class admin
+    {
+        //youssef
+        //pro
+
+    }
+}
